2e5647555007ca3c52503c24e546575026c3159b
cheetahspots01 <112513827+752986@users.noreply.github.com>
2023-01-19 21:58:22 -0700

    Add barrier creation functionality

--- cover.py
+++ cover.py
@@ -1,5 +1,73 @@
 import pygame
+from pygame.math import Vector2
 from gameObject import GameObject
+from sprite import Sprite
+from utility import is_hit
 
-class CoverTile(GameObject):
+
+# the 4 stages of damage for a tile of cover
+COVER_IMAGES = [
+    pygame.image.load("invaders_imgs/barrier_0.png").convert_alpha(),
+    pygame.image.load("invaders_imgs/barrier_1.png").convert_alpha(),
+    pygame.image.load("invaders_imgs/barrier_2.png").convert_alpha(),
+    pygame.image.load("invaders_imgs/barrier_3.png").convert_alpha(),
+]
+
+# the layout of each barrier
+BARRIER_SHAPE = [
+    [False, True, True, False],
+    [True, True, True, True],
+    [True, True, True, True],
+    [True, False, False, True],
+]
+
+# an individual tile of a barrier
+class CoverTile(Sprite):
+    health: int
+
+    def __init__(self, pos: Vector2, size: Vector2 | None):
+        super().__init__(pos, COVER_IMAGES[0], size)
+        self.health = 4
+
+    def update(self, game_objects: list[GameObject], delta: float):
+        # if a projectile is colliding with the tile, take damage
+        if is_hit(self, game_objects):
+            self.health -= 1
+
+            # if the tile is dead, remove it; otherwise, progress its stage of damage
+            if self.health <= 0:
+                self.should_delete = True
+            else:
+                self.replace_image(
+                    COVER_IMAGES[4 - self.health], Vector2(self.rect.size))
+
+
+# create the CoverTiles that form a single barrier 
+def generate_barrier(tile_size: float, position: Vector2) -> list[CoverTile]:
+    tiles: list[CoverTile] = []
+
+    for (y, row) in enumerate(BARRIER_SHAPE):
+        for (x, val) in enumerate(row):
+            if val:
+                tiles.append(CoverTile(
+                    position + Vector2(x * tile_size, -y * tile_size), Vector2(tile_size)))
+
+    return tiles
+
+
+# generate and fill in all of the needed barriers
+def fill_cover(add_to: list[GameObject], tile_size: float, height: float, screen_width: float, n_barriers: int):
+    barrier_width = tile_size * len(BARRIER_SHAPE[0]) # how wide should a barrier be
+    barrier_spacing = (screen_width - (barrier_width * n_barriers)) / n_barriers # how much space should there be between barriers
     
+    # create each barrier in the correct position and add it to the provided `add_to` array
+    for i in range(n_barriers):
+        add_to.extend(
+            generate_barrier(
+                tile_size,
+                Vector2(
+                    (barrier_spacing / 2) + (i * barrier_spacing),
+                    height
+                )
+            )
+        )
--- sprite.py
+++ sprite.py
@@ -3,18 +3,48 @@ from gameObject import GameObject
 
 
 class Sprite(GameObject):
-    def __init__(
-        self,
-        pos: pygame.Vector2,
-        image: pygame.surface.Surface,
-        image_size: pygame.Vector2 | None,
-    ):
-        self.image = pygame.transform.smoothscale(
-            image, image_size if image_size != None else (1, 1)
-        )
-        self.rect = image.get_rect()
+    def __init__(self, pos: pygame.Vector2, image: pygame.surface.Surface, image_size: pygame.Vector2 | None, alpha: bool = False):
+        self.pos = pos
+
+        # resize the image if a size was provided
+        if image_size == None:
+            self.image = image
+        else:
+            self.image = pygame.transform.smoothscale(image, image_size)
+
+        # convert the image into the correct format for the display surface
+        if alpha:
+            self.image = self.image.convert_alpha()
+        else:
+            self.image = self.image.convert()
+
+        # calculate the bounding rect
+        self.rect = self.image.get_rect()
         self.rect.center = (int(pos.x), int(pos.y))
         super().__init__(self.rect)
+
+    def replace_image(self, new_image: pygame.surface.Surface, image_size: pygame.Vector2 | None, alpha: bool = False):
+        '''Replaces the `Sprite`'s current image with a new one'''
+
+        # resize the image if a size was provided
+        if image_size == None:
+            self.image = new_image
+        else:
+            self.image = pygame.transform.smoothscale(new_image, image_size)
+        
+        # convert the image into the correct format for the display surface
+        if alpha:
+            self.image = self.image.convert_alpha()
+        else:
+            self.image = self.image.convert()
+
+        # calculate the new bounding rect
+        old_pos = self.rect.center
+        self.rect = self.image.get_rect()
+        self.rect.center = old_pos
+
+    def update(self, game_objects: list["GameObject"], delta: float):
+        self.rect.center = (int(self.pos.x), int(self.pos.y))
 
     def draw(self, surface: pygame.surface.Surface):
         self.image.blit(surface, self.rect)
