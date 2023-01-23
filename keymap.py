824f30937e3d4553e50a39c59fcbf6422c767373
Email 6 <loserkid0006@outlook.com>
2023-01-23 14:00:48 -0700

    and left and right

--- keymap.py
+++ keymap.py
@@ -1,9 +1,7 @@
 import pygame
 
-# LEFT = pygame.K_LEFT
-# RIGHT = pygame.K_RIGHT
-LEFT = pygame.K_a
-RIGHT = pygame.K_d
+LEFT = [pygame.K_LEFT, pygame.K_a]
+RIGHT = [pygame.K_RIGHT, pygame.K_d]
 FIRE = pygame.K_SPACE
 
 QUIT = pygame.K_LCTRL
--- player.py
+++ player.py
@@ -14,26 +14,32 @@ class Player(Sprite):
     can_shoot: bool
 
     def on_projectile_deleted(self, event: Event):
-        assert type(event) is projectile.ProjectileDeleted # this is needed to appease the type checker
+        assert (
+            type(event) is projectile.ProjectileDeleted
+        )  # this is needed to appease the type checker
         if event.projectile.owner is self:
             self.can_shoot = True
 
     def __init__(self, game_state: GameState, pos: pygame.Vector2):
         super().__init__(pos, pygame.image.load("invaders_imgs/player.png"), None, True)
         self.can_shoot = True
-        game_state.event_handler.subscribe(self, projectile.ProjectileDeleted, self.on_projectile_deleted)
+        game_state.event_handler.subscribe(
+            self, projectile.ProjectileDeleted, self.on_projectile_deleted
+        )
 
     def update(self, game_state: GameState, delta: float):
         keys = pygame.key.get_pressed()
-        if keys[keymap.LEFT]:
+        if keys[keymap.LEFT[0]] or keys[keymap.LEFT[1]]:
             self.rect.move_ip(-SPEED * delta, 0)
-        if keys[keymap.RIGHT]:
+        if keys[keymap.RIGHT[0]] or keys[keymap.RIGHT[1]]:
             self.rect.move_ip(SPEED * delta, 0)
 
         if keys[keymap.FIRE] and self.can_shoot:
-            game_state.game_objects.append(projectile.Projectile(game_state, pygame.Vector2(0, -1000), self))
+            game_state.game_objects.append(
+                projectile.Projectile(game_state, pygame.Vector2(0, -1000), self)
+            )
             self.can_shoot = False
 
         # projectile collision
         if utility.is_hit(self, game_state.game_objects):
-            pass # code for damage here
+            pass  # code for damage here
